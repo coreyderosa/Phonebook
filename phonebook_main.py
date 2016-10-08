44cb5c8b1f462c66ee48b627fec4193f41a2d865
coreyderosa <coreyderosa@gmail.com>
2016-10-08 12:55:07 -0600

    Initial commit of phonebook project
    phonebook_main- define the ParentWindow class and main program setup

---
+++ phonebook_main.py
@@ -0,0 +1,11 @@
+#Python Version: 3.5.2
+#
+#Author:                Corey DeROsa
+#
+#Purpose:             Create a phone book app that allows the user to add entries as well as retrieve entries
+#
+#Python Course:   Items 49-51
+
+from tkinter import *
+from tkinter import tk
+
