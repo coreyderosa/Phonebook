889c85613ead48dd2e1b705766e6b6340fa4e085
coreyderosa <coreyderosa@gmail.com>
2016-10-08 13:09:56 -0600

    Updated the tkinter imprt
    received an error using tk.  Updated to ttk and module loaded fine

--- phonebook_main.py
+++ phonebook_main.py
@@ -7,5 +7,32 @@
 #Python Course:   Items 49-51
 
 from tkinter import *
-from tkinter import tk
+from tkinter import ttk
 
+#Importing my other modules needed to run the phone book
+#import phonebook_gui
+#import phonebook_func
+
+#Setting up the class
+class ParentWIndow(Frame):
+    def __init__(self, master, *args, **kwargs):
+        Frame.__init__(self, master, *args, **kwargs)
+
+        #define the master frame config
+        self.master = master
+        self.master.minsize(500, 300) #height, width
+        self.master.maxsize(500, 300)
+        phonebook_func.center_window(self, 500, 300) #center_window will center the GUI window in the center of the screen
+        self.master.titile('The Tkinter Phonebook')
+        self.master.configure(bg='F0F0F0') #bg = background
+        #WM_DELETE_WINDOW is 'X' in upper corner of window that closes window
+        self.master.protocol('WM_DELETE_WINDOW', lambda: phonebook_func.ask_quit(self))
+        arg = self.master
+        phonebook_gui.load_gui(self) #calls the load_gui function from the phonebook_gui file
+
+
+if __name__== '__main__':
+    root = Tk()
+    App = ParentWindow(root)
+    root.mainloop()
+        
